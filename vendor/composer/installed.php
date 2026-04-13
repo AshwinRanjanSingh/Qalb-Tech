81833895651e25aabce0fb7d29e2d6097ab236c4
Ashwin Singh <ashwinranjan.singh@gmail,com>
2026-04-13 18:00:37 +0530

    force added files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'qalbtech/qalbtech',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c4a336f74a1ddadbc0b0f2ea9011e20d5e0a08b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'qalbtech/qalbtech' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c4a336f74a1ddadbc0b0f2ea9011e20d5e0a08b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
